99b6dd19c8df551f0e1a00973df330ce28e190ce
Mate Gonczi <gamte19@gmail.com>
2016-11-16 15:10:55 +0100

    Implement Cart interface, CartImpl class

---
+++ src/main/java/com/codecool/shop/dao/Cart.java
@@ -0,0 +1,8 @@
+package com.codecool.shop.dao;
+
+
+import com.codecool.shop.model.Product;
+
+public interface Cart {
+    void addProduct(Product product);
+}
---
+++ src/main/java/com/codecool/shop/dao/implementation/CartImpl.java
@@ -0,0 +1,11 @@
+package com.codecool.shop.dao.implementation;
+
+
+import com.codecool.shop.dao.Cart;
+
+import java.util.List;
+
+public class CartImpl implements Cart {
+    private List lineItems<> = new List();
+
+}
