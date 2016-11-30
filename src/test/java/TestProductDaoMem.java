4cfa192acadcf821ead1b49a7fbb22ce16de01ad
Lőrincz Szilvia <lorszil@Lorinczs-MacBook-Pro.local>
2016-11-30 14:25:04 +0100

    creating setup to TestProductDaoMem

---
+++ src/test/java/TestProductDaoMem.java
@@ -0,0 +1,61 @@
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.Supplier;
+import com.codecool.shop.model.ProductCategory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNull;
+
+public class TestProductDaoMem {
+
+    private Product product1;
+    private Product product2;
+    private Product product3;
+
+    SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+    private Supplier lenovo;
+    private Supplier amazon;
+    ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+    private ProductCategory tablet;
+    private ProductCategory notebook;
+    ProductDao productDataStore = ProductDaoMem.getInstance();
+
+
+
+    @Before
+    public void setUp() {
+
+
+        lenovo = new Supplier("Lenovo", "Computers");
+        supplierDataStore.add(lenovo);
+        amazon = new Supplier("Amazon", "Computers");
+        supplierDataStore.add(amazon);
+
+        tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
+        productCategoryDataStore.add(tablet);
+        notebook = new ProductCategory("Notebook", "Hardware", "A tablet computer, commonly shortened to tablet.");
+        productCategoryDataStore.add(notebook);
+
+
+        product1 = new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon);
+        productDataStore.add(product1);
+        product2 = new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo);
+        productDataStore.add(product2);
+        product3 = new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", notebook, amazon);
+        productDataStore.add(product3);
+
+    }
+
+
+}
