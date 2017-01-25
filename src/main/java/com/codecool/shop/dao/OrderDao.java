709c04e000b1e6c8bcf68d5232ed8aaba3c01c0c
Lőrincz Szilvia <lorszil@Lorinczs-MacBook-Pro.local>
2017-01-25 12:56:19 +0100

    adding more method

--- src/main/java/com/codecool/shop/dao/OrderDao.java
+++ src/main/java/com/codecool/shop/dao/OrderDao.java
@@ -3,6 +3,11 @@ package com.codecool.shop.dao;
 
 import com.codecool.shop.model.Order;
 
+import java.util.List;
+
 public interface OrderDao {
     void add(Order order);
+    Order find(int id);
+
+    List<Order> getAll();
 }
